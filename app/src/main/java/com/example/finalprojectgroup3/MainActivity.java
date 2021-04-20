bd10f8f03b9823651e0332842c07243a37669220
San Vu <san.vu@sjsu.edu>
2021-04-20 13:56:39 -0700

    Finished layouts

--- app/src/main/java/com/example/finalprojectgroup3/MainActivity.java
+++ app/src/main/java/com/example/finalprojectgroup3/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.finalprojectgroup3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void start(View view) {
+        if (view.getId() == R.id.start) {
+            Intent call = new Intent(this,UploadActivity.class);
+            startActivity(call);
+        }
     }
 }
